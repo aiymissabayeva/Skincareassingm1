579878d1641b907d68c74db4cfdeab7c3aa27e01
ajym <fxynszrbpk@privaterelay.appleid.com>
2026-09-12 01:40:29 +0500

    AcneBuilder and implementing methods

---
+++ src/AcneSkincareBuilder.java
@@ -0,0 +1,37 @@
+public class AcneSkincareBuilder implements SkincareBuilder{
+    private String name;
+    private String brand = "Unknown";
+    private String skinType = "Unknown";
+    private String mainIngredient = "Salicylic Acid";
+    private boolean spf = false;
+
+    @Override
+    public SkincareBuilder setName(String name) {
+        return null;
+    }
+
+    @Override
+    public SkincareBuilder setBrand(String brand) {
+        return null;
+    }
+
+    @Override
+    public SkincareBuilder setSkinType(String skinType) {
+        return null;
+    }
+
+    @Override
+    public SkincareBuilder setMainIngredient(String mainIngredient) {
+        return null;
+    }
+
+    @Override
+    public SkincareBuilder setSpf(boolean spf) {
+        return null;
+    }
+
+    @Override
+    public SkincareProduct build() {
+        return null;
+    }
+}
