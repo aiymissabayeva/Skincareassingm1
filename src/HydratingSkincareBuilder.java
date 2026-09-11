326f9c9de118ba7c969c744e7655c381a3a9a64f
ajym <fxynszrbpk@privaterelay.appleid.com>
2026-09-11 15:29:03 +0500

    Adding important and default values; Using implementing methods

---
+++ src/HydratingSkincareBuilder.java
@@ -0,0 +1,37 @@
+public class HydratingSkincareBuilder implements SkincareBuilder{
+    private String name;
+    private String brand = "Not choosen";
+    private String skinType = "Not choosen";
+    private String mainIngredient = "Unknown";
+    private boolean spf = false;
+
+    @Override
+    public SkincareBuilder setName(String name) {
+        return null;
+    }
+
+    @Override
+    public SkincareBuilder setBrand(String brand) {
+        return null;
+    }
+
+    @Override
+    public SkincareBuilder setSkinType(String skinType) {
+        return null;
+    }
+
+    @Override
+    public SkincareBuilder setMainIngredient(String mainIngredient) {
+        return null;
+    }
+
+    @Override
+    public SkincareBuilder setSpf(boolean spf) {
+        return null;
+    }
+
+    @Override
+    public SkincareProduct build() {
+        return null;
+    }
+}
