badd785a7be5479e911144d235ea8d58e25dce7e
ajym <fxynszrbpk@privaterelay.appleid.com>
2026-09-11 15:50:44 +0500

    Added constructor

--- src/SkincareProduct.java
+++ src/SkincareProduct.java
@@ -5,5 +5,11 @@ public class SkincareProduct {
     private String mainIngredient;
     private boolean spf;
 
-
+    public SkincareProduct(String name, String brand, String skinType, String mainIngredient, boolean spf){
+        this.name = name;
+        this.brand = brand;
+        this.skinType = skinType;
+        this.mainIngredient = mainIngredient;
+        this.spf = spf;
+    }
 }
