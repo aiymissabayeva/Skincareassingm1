b077bfa0b3eeec213fcc246856def020113f1dcd
ajym <fxynszrbpk@privaterelay.appleid.com>
2026-09-12 19:39:04 +0500

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         SkincareDirector director = new SkincareDirector();
-        SkincareProduct hydratingProduct = director.createHydratingProduct("Hydrating Cream");
+        SkincareProduct hydratingProduct = director.createHydratingProduct("Hydrating Cream", "Dry");
 
-        SkincareProduct acneProduct = director.createAcneProduct("Acne Serum");
+        SkincareProduct acneProduct = director.createAcneProduct("Acne Serum", true);
 
         System.out.println(hydratingProduct);
         System.out.println(acneProduct);
--- src/SkincareDirector.java
+++ src/SkincareDirector.java
@@ -1,12 +1,14 @@
 public class SkincareDirector {
-    public SkincareProduct createHydratingProduct(String name){
+    public SkincareProduct createHydratingProduct(String name, String skinType){
         return new HydratingSkincareBuilder()
                 .setName(name)
+                .setSkinType(skinType)
                 .build();
     }
-    public SkincareProduct createAcneProduct(String name){
+    public SkincareProduct createAcneProduct(String name, boolean spf){
         return new AcneSkincareBuilder()
                 .setName(name)
+                .setSpf(spf)
                 .build();
     }
 }
